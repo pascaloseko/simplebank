b3ee59003e4b6d7807ed9b4620c7727af8a633cd
pascaloseko <pascal.ondieki21@gmail.com>
2023-06-21 18:20:18 +0300

    add create transfer API + unit tests

---
+++ server/transfer.go
@@ -0,0 +1,67 @@
+package server
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/pkg/errors"
+	"github.com/simplebank/repo"
+)
+
+type transferRequest struct {
+	FromAccountID int64  `json:"from_account_id" binding:"required,min=1"`
+	ToAccountID   int64  `json:"to_account_id" binding:"required,min=1"`
+	Amount        int64  `json:"amount" binding:"required,gt=0"`
+	Currency      string `json:"currency" binding:"required,currency"`
+}
+
+func (s *Server) createTransfer(ctx *gin.Context) {
+	var req transferRequest
+	if err := ctx.ShouldBindJSON(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return
+	}
+
+	if !s.validAccount(ctx, req.FromAccountID, req.Currency) {
+		return
+	}
+
+	if !s.validAccount(ctx, req.ToAccountID, req.Currency) {
+		return
+	}
+
+	arg := repo.TransferTxParams{
+		FromAccountID: req.FromAccountID,
+		ToAccountID:   req.ToAccountID,
+		Amount:        req.Amount,
+	}
+
+	result, err := s.store.TransferTx(ctx, arg)
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return
+	}
+
+	ctx.JSON(http.StatusOK, result)
+}
+
+func (s *Server) validAccount(ctx *gin.Context, accountID int64, currency string) bool {
+	account, err := s.store.GetAccount(ctx, accountID)
+	if err != nil {
+		if errors.Is(err, repo.ErrRecordNotFound) {
+			ctx.JSON(http.StatusNotFound, errResponse(err))
+			return false
+		}
+
+		ctx.JSON(http.StatusInternalServerError, errResponse(err))
+		return false
+	}
+
+	if account.Currency != currency {
+		err := fmt.Errorf("account [%d] currency mismatch: %s vs %s", account.ID, account.Currency, currency)
+		ctx.JSON(http.StatusBadRequest, errResponse(err))
+		return false
+	}
+	return true
+}
