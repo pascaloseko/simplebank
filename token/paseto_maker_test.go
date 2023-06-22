62428b4a2f355e2e2fb7d90cecc8966964fdee31
pascaloseko <pascal.ondieki21@gmail.com>
2023-06-22 12:27:52 +0300

    add paseto test

---
+++ token/paseto_maker_test.go
@@ -0,0 +1,49 @@
+package token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/simplebank/internal/testutils"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPasetoMaker(t *testing.T) {
+	maker, err := NewPasetoMaker(testutils.RandomString(32))
+	require.NoError(t, err)
+
+	username := testutils.RandomOwner()
+	duration := time.Minute
+
+	issuedAt := time.Now()
+	expiredAt := issuedAt.Add(duration)
+
+	token, payload, err := maker.CreateToken(username, duration)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+
+	require.NotZero(t, payload.ID)
+	require.Equal(t, username, payload.Username)
+	require.WithinDuration(t, issuedAt, payload.IssuedAt, time.Second)
+	require.WithinDuration(t, expiredAt, payload.ExpiredAt, time.Second)
+}
+
+func TestExpiredPasetoToken(t *testing.T) {
+	maker, err := NewPasetoMaker(testutils.RandomString(32))
+	require.NoError(t, err)
+
+	token, payload, err := maker.CreateToken(testutils.RandomOwner(), -time.Minute)
+	require.NoError(t, err)
+	require.NotEmpty(t, token)
+	require.NotEmpty(t, payload)
+
+	payload, err = maker.VerifyToken(token)
+	require.Error(t, err)
+	require.EqualError(t, err, ErrExpiredToken.Error())
+	require.Nil(t, payload)
+}
